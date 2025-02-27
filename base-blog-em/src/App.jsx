974ba5f0aace739616c387d7b8c3dffa451f1c77
Natalia Macias <38480860+NataJenkins@users.noreply.github.com>
2025-02-27 14:44:01 -0600

    uppdate query provider

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -2,12 +2,12 @@ import { Posts } from "./Posts";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const QueryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 function App() {
     return (
         // provide React Query client to App
-        <QueryClientProvider client={QueryClient}>
+        <QueryClientProvider client={queryClient}>
             <div className="App">
                 <h1>Blog &apos;em Ipsum</h1>
                 <Posts />
