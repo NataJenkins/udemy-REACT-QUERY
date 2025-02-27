d986e4a96649fbf17db925e72f0845340be778e5
Natalia Macias <38480860+NataJenkins@users.noreply.github.com>
2025-02-26 20:04:01 -0600

    Adding Query client provider

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,14 +1,19 @@
 import { Posts } from "./Posts";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const QueryClient = new QueryClient();
 
 function App() {
-  return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
-  );
+    return (
+        // provide React Query client to App
+        <QueryClientProvider client={QueryClient}>
+            <div className="App">
+                <h1>Blog &apos;em Ipsum</h1>
+                <Posts />
+            </div>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
