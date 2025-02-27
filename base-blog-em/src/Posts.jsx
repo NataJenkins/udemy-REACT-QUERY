91af567df73b64e99b6d8d4d21d7fedf4d6d68fe
Natalia Macias <38480860+NataJenkins@users.noreply.github.com>
2025-02-27 15:03:53 -0600

    Handling Loading and Error states

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -10,11 +10,21 @@ export function Posts() {
     const [selectedPost, setSelectedPost] = useState(null);
 
     // replace with useQuery
-    const { data } = useQuery({
+    const { data, isError, error, isLoading } = useQuery({
         queryKey: ["posts"],
         queryFn: fetchPosts,
     });
 
+    if (isLoading) {
+        return <h3>Loading...</h3>;
+    }
+    if (isError) {
+        return (
+            <h3>
+                Error fetching posts Error: <p>{error.toString()}</p>
+            </h3>
+        );
+    }
     return (
         <>
             <ul>
